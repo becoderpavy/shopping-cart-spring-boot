22942bdd839721381fefefef41d9de0917487dd1
Pabitra Das <daspabitra55@gmail.com>
2024-04-16 07:57:26 +0530

    Product Edit module implement

--- src/main/java/com/ecom/service/impl/ProductServiceImpl.java
+++ src/main/java/com/ecom/service/impl/ProductServiceImpl.java
@@ -1,10 +1,17 @@
 package com.ecom.service.impl;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ecom.model.Product;
 import com.ecom.repository.ProductRepository;
@@ -35,6 +42,50 @@ public class ProductServiceImpl implements ProductService {
 			return true;
 		}
 		return false;
+	}
+
+	@Override
+	public Product getProductById(Integer id) {
+		Product product = productRepository.findById(id).orElse(null);
+		return product;
+	}
+
+	@Override
+	public Product updateProduct(Product product, MultipartFile image) {
+
+		Product dbProduct = getProductById(product.getId());
+
+		String imageName = image.isEmpty() ? dbProduct.getImage() : image.getOriginalFilename();
+
+		dbProduct.setTitle(product.getTitle());
+		dbProduct.setDescription(product.getDescription());
+		dbProduct.setCategory(product.getCategory());
+		dbProduct.setPrice(product.getPrice());
+		dbProduct.setStock(product.getStock());
+		dbProduct.setImage(imageName);
+		
+		Product updateProduct = productRepository.save(dbProduct);
+
+		if (!ObjectUtils.isEmpty(updateProduct)) {
+
+			if (!image.isEmpty()) {
+
+				try {
+					File saveFile = new ClassPathResource("static/img").getFile();
+
+					Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + "product_img" + File.separator
+							+ image.getOriginalFilename());
+					Files.copy(image.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
+			return product;
+		}
+
+		return null;
 	}
 
 }
